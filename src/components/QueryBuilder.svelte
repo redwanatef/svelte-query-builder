9d2ca1d03e0f6e74a5ee30ab91464e1fbc2cbc20
redwanatef <redwan.atef.02@gmail.com>
2025-05-16 19:08:48 +0300

    initialize the exporter

--- src/components/QueryBuilder.svelte
+++ src/components/QueryBuilder.svelte
@@ -3,6 +3,9 @@
   import { defaultConfig, defaultQuery } from "../config";
   import type { QueryBuilderConfig, RuleSet } from "../types";
   import QueryBuilderGroup from "./QueryBuilderGroup.svelte";
+  import { convertToLuceneQuery } from "../utils/LuceneConverter";
+  import { convertToSQL } from "../utils/SQLConverter";
+  import { convertToMongoDB } from "../utils/MongoConverter";
 
   let {
     config = defaultConfig,
@@ -15,8 +18,36 @@
   let queryState = $state(query);
 
   setContext("config", config);
+
+  let selectedQuery = $state("lucene");
+
+  let exportedQuery = $state();
+  $effect(() => {
+    if (selectedQuery === "lucene") {
+      exportedQuery = convertToLuceneQuery(queryState);
+    } else if (selectedQuery === "sql") {
+      exportedQuery = convertToSQL(queryState);
+    } else if (selectedQuery === "mongo") {
+      exportedQuery = JSON.stringify(convertToMongoDB(queryState), null, 2);
+    }
+  });
 </script>
 
 <div class="min-w-[90%] lg:min-w-[60%] max-w-fit mx-auto">
   <QueryBuilderGroup child={false} currentNode={queryState} />
+
+  <div>
+    Convert to:
+    <select bind:value={selectedQuery} class="border ml-4 p-1">
+      <option value="lucene">Lucene</option>
+      <option value="sql">SQL</option>
+      <option value="mongo">Mongo</option>
+    </select>
+
+    <div class="mt-3 bg-gray-300 p-4">
+      <div class="w-[90%]">
+        {exportedQuery}
+      </div>
+    </div>
+  </div>
 </div>
